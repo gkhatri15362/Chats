7c632bc0555eddea3d7170592a3729c184ae2236
gkhatri15362 <gkhatri15362@gmail.com>
2025-06-23 16:09:39 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// src/app/layout.tsx
+import './globals.css'
+
+export const metadata = {
+  title: 'Chats App',
+  description: 'Chat with people easily!',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
